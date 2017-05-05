fbd49e225cf279f2b4f147a93af590dadfee52fe
llh <850360626@qq.com>
2017-05-05 11:48:58 +0800

    change name

--- app/src/main/java/io/stuhua/androiddrawable/mvp/ui/SecondActivity.java
+++ app/src/main/java/io/stuhua/androiddrawable/mvp/ui/SecondActivity.java
@@ -4,14 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import io.stuhua.androiddrawable.R;
 
 /**
  * Created by liulh on 2017/5/5 11:08 星期五
  */
 
 public class SecondActivity extends AppCompatActivity {
+    private LayoutInflater mInflater;
+    private View mOuterView;
+    private View mInnerView;
+    private LinearLayout mParentLL;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mParentLL = (LinearLayout) findViewById(R.id.parent);
+        mInflater = LayoutInflater.from(this);
+        mInnerView = mInflater.inflate(R.layout.textview, null, false);
+        mParentLL.addView(mInnerView);
     }
 }
---
+++ inflater的三个参数.md
@@ -0,0 +1,7 @@
+<pre>
+    mInflater = LayoutInflater.from(this);
+    mInnerView = mInflater.inflate(R.layout.textview, mParentLL, false);
+    mParentLL.addView(mInnerView);
+</pre>
+
+**id  dump UI 图片为parent->text，text的参数起作用**
